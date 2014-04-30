3e5c110062daeb2fbd4356cf5424bb5fc882751d
Frankie <orangeprince@hushmail.com>
2014-04-30 18:31:49 +0200

    rails new demo-vacuum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoVacuum::Application.config.secret_key_base = 'bea1af95f13e8b96cebe2b5e27756ddb9cf3793c661bcbcf8707475fb97174ce187349013fc04c57e7f0af593874dcb3cf8b3baca5fdc1ac61409c252622dafd'
